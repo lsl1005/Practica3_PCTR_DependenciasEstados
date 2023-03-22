4ac7bcfd8d0cf649f679da6e65bba86443a101d8
lsl1005 <lsl1005@alu.ubu.es>
2023-03-22 01:51:43 +0000

    Creación hilos de salida

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,25 +1,19 @@
 package src.p03.c01;
 
 public class SistemaLanzador {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';		
 		System.out.println("¡Parque abierto!");
 		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) {			
+			String puerta = ""+((char) (letra_puerta++));			
 			// Creacion de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
+			// Creacion de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salidas).start();
 		}
 	}	
 }
