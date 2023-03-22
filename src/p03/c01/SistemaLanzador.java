6b4f32eb39ee55892aea3fa333abee3f8ba61f7c
lsl1005 <lsl1005@alu.ubu.es>
2023-03-22 01:11:08 +0000

    Test

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -4,15 +4,14 @@ public class SistemaLanzador {
 	public static void main(String[] args) {
 		
 		IParque parque = new Parque(); // TODO
-		char letra_puerta = 'A';
-		
-		System.out.println("¡Parque abierto!");
+		char letra_puerta = 'A';		
+		System.out.println("�Parque abierto!");
 		
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
 			
 			String puerta = ""+((char) (letra_puerta++));
 			
-			// Creación de hilos de entrada
+			// Creacion de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
 			
